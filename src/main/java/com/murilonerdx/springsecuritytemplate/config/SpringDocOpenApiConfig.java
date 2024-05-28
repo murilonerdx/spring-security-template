147cfdd824514a06f3148f559607d01e0dba2d94
prinln <mu-silva@outlook.com>
2024-05-27 21:10:30 -0300

    fix: ajustando header para pegar informações do login bearer

--- src/main/java/com/murilonerdx/springsecuritytemplate/config/SpringDocOpenApiConfig.java
+++ src/main/java/com/murilonerdx/springsecuritytemplate/config/SpringDocOpenApiConfig.java
@@ -1,9 +1,9 @@
 package com.murilonerdx.springsecuritytemplate.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,30 +12,16 @@ import org.springframework.context.annotation.Configuration;
 public class SpringDocOpenApiConfig {
 
 	@Bean
-	public OpenAPI openAPI() {
+	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
-				.components(
-						new io.swagger.v3.oas.models.Components()
-								.addSecuritySchemes("security_auth", securityConfiguration()))
-				.info(
-						new Info()
-								.title("REST API - AUTH")
-								.description("API autenticação")
-								.version("v1")
-								.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
-								.contact(new Contact().name("Murilo P.S").email("mu-silva@outlook.com"))
-				);
+				.info(new Info().title("API Documentation").version("1.0.0"))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(new Components()
+						.addSecuritySchemes("bearerAuth", new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")
+								.in(SecurityScheme.In.HEADER)
+								.name("Authorization")));
 	}
-
-	@Bean
-	SecurityScheme securityConfiguration(){
-		return new SecurityScheme()
-				.description("Informar o token JWT")
-				.type(SecurityScheme.Type.HTTP)
-				.in(SecurityScheme.In.HEADER)
-				.name("security_auth")
-				.bearerFormat("JWT")
-				.scheme("bearer");
-	}
-
 }
